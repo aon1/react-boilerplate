fe36b74dc510a3119915e5de8e3a66da86e53a08
Arlen Nascimento <arlen.nascimento@gmail.com>
2018-02-08 20:39:39 -0400

    changing stuff

--- public/bundle.js
+++ public/bundle.js
@@ -26637,7 +26637,7 @@
 		return React.createElement(
 			'h3',
 			null,
-			'About component'
+			'About'
 		);
 	};
 
@@ -26655,7 +26655,7 @@
 		return React.createElement(
 			'h3',
 			null,
-			'Example component'
+			'Examples'
 		);
 	};
 
