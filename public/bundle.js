b0c4c2e216ee6c1620b55aea926dcc11ec8ddb24
Arlen Nascimento <arlen.nascimento@gmail.com>
2018-02-08 21:23:21 -0400

    styling examples

--- public/bundle.js
+++ public/bundle.js
@@ -24924,11 +24924,14 @@
 			null,
 			React.createElement(Nav, null),
 			React.createElement(
-				'h2',
-				null,
-				'Main component'
-			),
-			props.children
+				'div',
+				{ className: 'row' },
+				React.createElement(
+					'div',
+					{ className: 'columns medium-6 large-4 small-centered' },
+					props.children
+				)
+			)
 		);
 	};
 
@@ -26765,11 +26768,45 @@
 
 	var React = __webpack_require__(8);
 
+	var _require = __webpack_require__(166),
+	    Link = _require.Link;
+
 	var Examples = function Examples(props) {
 		return React.createElement(
-			'h3',
+			'div',
 			null,
-			'Examples'
+			React.createElement(
+				'h1',
+				{ className: 'text-center' },
+				'Examples'
+			),
+			React.createElement(
+				'p',
+				null,
+				'Here are a few examples location to try out:'
+			),
+			React.createElement(
+				'ol',
+				null,
+				React.createElement(
+					'li',
+					null,
+					React.createElement(
+						Link,
+						{ to: '/?location=Manaus' },
+						'Manaus, AM'
+					)
+				),
+				React.createElement(
+					'li',
+					null,
+					React.createElement(
+						Link,
+						{ to: '/?location=Rio' },
+						'Rio, RJ'
+					)
+				)
+			)
 		);
 	};
 
